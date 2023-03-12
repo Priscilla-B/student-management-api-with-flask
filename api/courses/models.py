0c81805714f0ee84dadbf7e48df9c950f1ee001f
Priscilla Baah <pyfbaah@gmail.com>
2023-03-12 23:26:00 +0000

    update course model

--- api/courses/models.py
+++ api/courses/models.py
@@ -5,8 +5,7 @@ class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.String(20), nullable=False)
     name = db.Column(db.String(150), nullable=False)
-    teacher = db.Column(db.String(150), nullable=False)
-    username = db.Column(db.String(150), unique=True, nullable=False)
+    teacher = db.relationship(db.String(150), nullable=False)
     students = db.relationship('Student', backref='course')
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
    
