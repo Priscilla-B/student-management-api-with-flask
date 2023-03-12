1ae06a6eeaed6aa314df7421c78e5e30fcfddbe7
Priscilla Baah <pyfbaah@gmail.com>
2023-03-12 23:24:58 +0000

    initial auth views

--- api/auth/views.py
+++ api/auth/views.py
@@ -3,3 +3,19 @@ from flask_restx import Namespace, Resource
 auth_namespace = Namespace(
     'auth',
     description='a namespace for authentication logic')
+
+
+@auth_namespace.route('/register')
+class Register(Resource):
+
+    def post(self):
+        pass
+
+    def get(self):
+        return {'message':'hello auth!'}
+    
+
+@auth_namespace.route('/login')
+class Login(Resource):
+    def post(self):
+        pass
