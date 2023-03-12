d96c164a354aba6182a3682b0295d3dcc72f30ed
Priscilla Baah <pyfbaah@gmail.com>
2023-03-12 21:16:38 +0000

    register app routes

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_restx import Api
 
-def create_app():
+from .auth.views import auth_namespace
+from .courses.views import courses_namespace
+from .grading.views import grading_namespace
+from .students.views import students_namespace
+
+from .config.config import config_dict
+
+
+def create_app(config=config_dict['dev']):
     app = Flask(__name__)
+
+    app.config.from_object(config)
+
+    api = Api(app)
+
+    api.add_namespace(auth_namespace, path='/auth')
+    api.add_namespace(courses_namespace, path='/course')
+    api.add_namespace(grading_namespace, path='/')
+    api.add_namespace(students_namespace, path='/student')
 
     return app
