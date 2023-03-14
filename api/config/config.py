e9934d01da1bbd960b1c8a0cb67da267dc0e311c
Priscilla Baah <pyfbaah@gmail.com>
2023-03-14 23:44:15 +0000

    add sqlalchemy db settings

--- api/config/config.py
+++ api/config/config.py
@@ -1,13 +1,24 @@
 import os
 from decouple import config
 
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+# get the directory of file being edited
+
 class Config:
     SECRET_KEY = config('SECRET_KEY', 'secret')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #  prevent sql alchemy from setting up a session to 
+    # track inserts, updates, and deletes for models
     pass
 
 
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_ECHO = True
+    # to log queries generated
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3')
 
 
 class TestConfig(Config):
