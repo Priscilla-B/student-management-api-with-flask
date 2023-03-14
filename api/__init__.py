218b066ce85d8e4ee652a76f36d03a31bc0b3e81
Priscilla Baah <pyfbaah@gmail.com>
2023-03-14 23:43:18 +0000

    add migration settings

--- api/__init__.py
+++ api/__init__.py
@@ -6,14 +6,21 @@ from .courses.views import courses_namespace
 from .grading.views import grading_namespace
 from .students.views import students_namespace
 from .teachers.views import teachers_namespace
+from .auth.models import User
 
 from .config.config import config_dict
+from .utils import db, migrate
+
+
 
 
 def create_app(config=config_dict['dev']):
     app = Flask(__name__)
 
     app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     api = Api(app)
 
@@ -22,5 +29,15 @@ def create_app(config=config_dict['dev']):
     api.add_namespace(grading_namespace, path='')
     api.add_namespace(students_namespace, path='/student')
     api.add_namespace(teachers_namespace, path='/teacher')
+
+    with app.app_context():
+        db.create_all()
+
+    @app.shell_context_processor
+    def make_shell_context():
+        return {
+            'db':db,
+            'User':User
+        }
 
     return app
