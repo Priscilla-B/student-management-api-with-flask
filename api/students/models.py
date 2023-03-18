0816a17da03b953b1d116559776c8ea39d0bedb5
Priscilla Baah <pyfbaah@gmail.com>
2023-03-18 05:02:36 +0000

    add model save method

--- api/students/models.py
+++ api/students/models.py
@@ -1,11 +1,17 @@
-from utils import db
+from api.utils import db
 from sqlalchemy.sql import func
 
 class Student(db.Model):
+    __tablename__ = 'students'
+
     id = db.Column(db.Integer(), primary_key=True)
     student_id = db.Column(db.String(20), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     
     def __repr__(self):
         return f'{self.username}'
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
