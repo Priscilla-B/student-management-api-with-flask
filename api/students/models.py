8ec6a5a767079ef435626b5a439c20b0fa255929
Priscilla Baah <pyfbaah@gmail.com>
2023-03-18 22:35:54 +0000

    update user fk connection

--- api/students/models.py
+++ api/students/models.py
@@ -6,7 +6,7 @@ class Student(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True)
     student_id = db.Column(db.String(20), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     
     def __repr__(self):
