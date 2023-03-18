2fff0e7b26de65d7634e45fa05084c2c152c31fe
Priscilla Baah <pyfbaah@gmail.com>
2023-03-18 22:34:26 +0000

    add admin creation cli command

---
+++ manage.py
@@ -0,0 +1,49 @@
+import getpass
+
+from flask.cli import FlaskGroup
+from werkzeug.security import generate_password_hash
+
+from api.auth.models import User, Role
+from app import app
+
+cli = FlaskGroup(app)
+
+
+@cli.command('create_admin')
+def create_admin():
+    """
+    Creates an admin user from cli
+    """
+    first_name = input("Enter first name: ")
+    last_name = input("Enter last name: ")
+    username = input("Enter username: ")
+    email = input("Enter email address: ")
+    password = getpass.getpass("Enter password: ")
+    confirm_password = getpass.getpass("Enter password again: ")
+    if password != confirm_password:
+        print("Passwords don't match")
+        return 1
+    
+    try:
+        user = User(
+            first_name=first_name,
+            last_name=last_name, 
+            username=username,  
+            email=email,
+            password=generate_password_hash(password),
+            is_staff=True,
+            is_active=True
+        )
+    
+        role = Role(name='admin')
+        role.save()
+        user.role = [role]
+        user.save()
+    except Exception:
+        print(Exception)
+
+
+if __name__ == "__main__":
+    cli()  
+
+     
