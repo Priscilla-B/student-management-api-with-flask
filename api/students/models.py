dfb7a0f0fc3a5cbb4637255dc952738f7964792a
Priscilla Baah <pyfbaah@gmail.com>
2023-03-21 00:40:08 +0000

    add model management methods

--- api/students/models.py
+++ api/students/models.py
@@ -9,8 +9,30 @@ class Student(db.Model):
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     
     def __repr__(self):
-        return f'{self.username}'
+        return f'{self.student_id}'
+    
+    def __getitem__(self, key):
+        """
+        To enable item assignments such as user["name"]
+        instead of user.name
+        """
+        return getattr(self, key)
+    
+    def __setitem__(self, key, value):
+        """
+        To enable item assignments such as student["name"]="new name{}
+        instead of student.name = "new name"
+        """
+        return setattr(self, key, value)
+    
+    @classmethod
+    def get_by_id(cls, student_id):
+        return cls.query.get_or_404(student_id)
     
     def save(self):
         db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
         db.session.commit()
