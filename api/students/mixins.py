dc735e2eb6508e14cfea52d6a0de784ef094c40c
Priscilla Baah <pyfbaah@gmail.com>
2023-03-21 00:38:59 +0000

    add student response mixin

---
+++ api/students/mixins.py
@@ -0,0 +1,13 @@
+from .models import Student
+
+class StudentResponseMixin(object):
+
+    def get_response(self, student_instance):
+        
+        response_data = {}
+        user = student_instance.user.as_dict()
+        user['student_id'] = student_instance.student_id
+        user['user_id'] = user['id']
+        response_data.update(user)
+
+        return response_data
