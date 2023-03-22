82d5005a5dca8ab3ee12ad9d79aa9f5e150e85b3
Priscilla Baah <pyfbaah@gmail.com>
2023-03-22 20:49:51 +0000

    create student course helper table

--- api/courses/models.py
+++ api/courses/models.py
@@ -1,12 +1,21 @@
-from utils import db
 from sqlalchemy.sql import func
+
+from api.utils import db
+
+
+StudentCourse = db.Table(
+    'student_course', db.Model.metadata,
+    db.Column('student_id', db.String(20), db.ForeignKey('student.student_id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'))
+)
+
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.String(20), nullable=False)
     name = db.Column(db.String(150), nullable=False)
-    teacher = db.relationship(db.String(150), nullable=False)
-    students = db.relationship('Student', backref='course')
+    teacher= db.Column(db.Integer, db.ForeignKey('user.id'))
+    # students = db.relationship('Student', secondary=StudentCourse)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
    
 
