1c4ee6708ee8aaa5672b85fdb2dd595919108420
Priscilla Baah <pyfbaah@gmail.com>
2023-03-24 06:58:29 +0000

    add create student serializer

--- api/students/serializers.py
+++ api/students/serializers.py
@@ -6,6 +6,38 @@ student_namespace = Namespace(
     description='a namespace for student logic')
 
 
+create_student_serializer = student_namespace.model(
+    'Student', 
+        {
+        'student_id':fields.String(
+            description='custom created student ID',
+            required = True
+        ),
+        'first_name': fields.String(
+            description='student first name',
+            required = True
+        ),
+        'last_name': fields.String(
+            description='student last name',
+            required = True
+        ),
+        'username': fields.String(
+            description='username of student in app',
+            required = True
+        ),
+        'email': fields.String(
+            description='student email',
+            required = True
+        ),
+        'password': fields.String(
+            description='password to student account',
+            required = True
+        )
+        }
+)
+
+
+
 student_serializer = student_namespace.model(
     'Student', 
         {
