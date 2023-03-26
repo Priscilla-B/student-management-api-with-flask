690265f96e27a63cb1d5ab5423a35f45811471a5
Priscilla Baah <pyfbaah@gmail.com>
2023-03-26 06:43:58 +0000

    add prod config and use python dotenv

--- api/config/config.py
+++ api/config/config.py
@@ -1,19 +1,20 @@
 import os
-from decouple import config
+from dotenv import load_dotenv
 from datetime import timedelta
 
 
+load_dotenv()
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 # get the directory of file being edited
 
 class Config:
-    SECRET_KEY = config('SECRET_KEY', 'secret')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #  prevent sql alchemy from setting up a session to 
     # track inserts, updates, and deletes for models
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(minutes=30)
-    JWT_SECRET_KEY = config('JWT_SECRET_KEY')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
 
 class DevConfig(Config):
@@ -29,7 +30,9 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3')
+    # still using sqlite in prod as I'll need a paid pythonanywhere account to access postgres
+    DEBUG = False
 
 
 config_dict = {
@@ -37,3 +40,5 @@ config_dict = {
     'prod':ProdConfig,
     'test':TestConfig
 }
+
+config_chosen = config_dict[os.environ.get('CONFIG_CHOSEN')]
