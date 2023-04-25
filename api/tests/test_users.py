2a7bc3bae24e92693d9ff6a9e48018e50c9d731e
Priscilla Baah <pyfbaah@gmail.com>
2023-04-25 22:07:08 +0000

    add tests for user creation and login

--- api/tests/test_users.py
+++ api/tests/test_users.py
@@ -1,12 +1,15 @@
 import unittest
+from werkzeug.security import generate_password_hash
 
 from .. import create_app
 from ..config.config import config_dict
+from ..auth.models import User
 from ..utils import db
 
 
+
 class UserTestCase(unittest.TestCase):
-    def setup(self):
+    def setUp(self):
         self.app = create_app(config=config_dict['test'])
         self.app_contxt = self.app.app_context()
         self.app_contxt.push()
@@ -14,7 +17,7 @@ class UserTestCase(unittest.TestCase):
         
         db.create_all()
 
-    def teardown(self):
+    def tearDown(self):
         """
         Destroys database  and removes app context after running tests
         """
@@ -23,3 +26,31 @@ class UserTestCase(unittest.TestCase):
         self.app_contxt.pop()
         self.app = None
         self.client = None
+
+
+    def test_user_creation(self):
+        data = {
+            "first_name":"Test",
+            "last_name":"User",
+            "username":"test_user",
+            "email":"test_user@gmail.com",
+            "password":"test123@",
+            "role":"teacher"
+        }
+
+        response = self.client.post('/auth/create_user', json=data)
+
+        user = User.query.filter_by(email=data["email"]).first()
+        assert user.username == data["username"]
+
+        assert response.status_code == 201
+
+    def test_login(self):
+        data = {
+            "email":"test_user@gmail.com",
+            "password":"test123@"
+        }
+        response = self.client.post('/auth/login', json=data)
+
+        assert response.status_code != 200
+        # user should not exist in database, so login should not be possible
